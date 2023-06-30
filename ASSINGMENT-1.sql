4a48ee5932b4df868eeb29783075cfaf1f92a618
Vinny2k2 <137828654+Vinny2k2@users.noreply.github.com>
2023-06-30 19:09:23 -0400

    Add files via upload

---
+++ ASSINGMENT-1.sql
@@ -0,0 +1,12 @@
+SELECT * FROM org.worker;
+SELECT * FROM org.worker;
+SELECT DEPARTMENT FROM org.worker; 
+SELECT left(FIRST_NAME,3) FROM org.worker; 
+SELECT * FROM org.worker NTorder by FIRST_NAME ASC AND DEPARTMENT DESC;
+SELECT * FROM org.worker
+where DEPARTMENT = Admin;
+ SELECT distinct DEPARTMENT FROM org.worker, lengthofDEPartment;
+SELECT * FROM org.worker
+WHERE SALARY >= 50000 and SALARY <= 100000;
+SELECT * FROM org.worker
+WHERE SALARY >= 50000 and SALARY <= 100000;
